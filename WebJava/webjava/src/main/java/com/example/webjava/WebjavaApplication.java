3e1377eed549d3a1a0a318e7a5dce4cb26aff83d
Phan Văn Phước <phuoc108480@donga.edu.vn>
2026-08-24 19:18:35 +0700

    Add files via upload

---
+++ WebJava/webjava/src/main/java/com/example/webjava/WebjavaApplication.java
@@ -0,0 +1,20 @@
+package com.example.webjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class WebjavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebjavaApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World!";
+    }
+}
